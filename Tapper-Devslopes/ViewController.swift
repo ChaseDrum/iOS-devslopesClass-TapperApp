95a45509ece07cf1e7387979077034ce0c66f046
ChaseDrum <chasedrum@mac.com>
2016-02-06 00:26:50 -0800

    Added files to Tapper game from Devslopes course to Github.

--- Tapper-Devslopes/ViewController.swift
+++ Tapper-Devslopes/ViewController.swift
@@ -9,17 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Variables/Properties
+    var MaxTaps = 0
+    var currentTaps = 0
+    
+    //Outlets
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    
+   
+    @IBOutlet weak var logoImage2: UIImageView!
+    @IBOutlet weak var playBtn2: UIButton!
+    
+    
+    
+    @IBOutlet weak var tapBtn:UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps++;
+        //++ just means plus 1, the alternative would have been curretTaps = currentTaps + 1
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    
+    @IBAction func playGameBtnPressed(sender:UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""{
+        // != means not equal to, so were saying if someone has typed something in and its blank, lets not doing anything otherwise run the code
+            logoImage.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+    
+            MaxTaps = Int(howManyTapsTxt.text!)!
+            
+            
+            updateTapsLbl()
+        }
+    }
+    
+    func restartGame() {
+        MaxTaps = 0
+        howManyTapsTxt.text = ""
+        currentTaps = 0
+        
+        logoImage.hidden = false
+        howManyTapsTxt.hidden = false
+        playBtn.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= MaxTaps {
+            return true
+        } else {
+            return false
+            }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
+        //We wrote this function to repeat from writing the same code from running repeatedly and makes it easier for us to use one point to make all modification.
     }
-
-
+    
 }
-
